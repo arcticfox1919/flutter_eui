b9c100ac724e681f198e7828cd94e52ea6361fe2
arcticfox1919 <arcticfox1919@163.com>
2019-06-15 13:43:59 +0800

    init

---
+++ lib/flutter_eui.dart
@@ -0,0 +1,12 @@
+library flutter_eui;
+
+import 'package:flutter/widgets.dart';
+
+class Button extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
+
